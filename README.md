fa6344be5727c71c53813c74452f729f8d67b728
xstaticxgpx <gabe@gabe.tech>
2023-08-07 04:27:40 -0400

    refactor to use a dict of arrays

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-Hello world!
+### Usage
+
+1. Define the sets in YAML
+
+2. Run the program
+```
+$ ./with_yaml.py
+Calculating project sets...
+
+set1 [45, 75, 45] = 165
+set2 [75, 85, 85, 85, 85, 75, 75, 45] = 610
+set3 [45, 75, 45, 55, 85, 55, 85] = 445
+set4 [75, 85, 55] = 215
+```
+
+Optionally with debugging:
+```
+$ ./with_yaml.py --debug
+Calculating project sets...
+
+1 start low cost city
+2 middle low cost city
+3 end low cost city
+{1: [45], 2: [75], 3: [45]}
+set1 [45, 75, 45] = 165
+1 1_full_day low cost city
+2 start high cost city
+pushed up against
+3 middle high cost city
+4 middle high cost city
+5 middle high cost city
+6 end high cost city
+6 start low cost city
+overlap
+7 middle low cost city
+8 end low cost city
+{1: [75], 2: [85], 3: [85], 4: [85], 5: [85], 6: [55, 75], 7: [75], 8: [45]}
+set2 [75, 85, 85, 85, 85, 75, 75, 45] = 610
+1 start low cost city
+2 middle low cost city
+3 end low cost city
+5 start high cost city
+6 middle high cost city
+7 end high cost city
+8 1_full_day high cost city
+{1: [45], 2: [75], 3: [45], 4: [], 5: [55], 6: [85], 7: [55], 8: [85]}
+set3 [45, 75, 45, 55, 85, 55, 85] = 445
+1 1_full_day low cost city
+1 1_full_day low cost city
+2 1_full_day high cost city
+2 start high cost city
+overlap
+3 end high cost city
+{1: [75, 75], 2: [85, 85], 3: [55]}
+set4 [75, 85, 55] = 215
+```
--- with_yaml.py
+++ with_yaml.py
@@ -1,59 +1,59 @@
 #!/usr/bin/python
 
 import yaml
+import sys
 
-def calculate(name, parsed):
-    start = min([project['start'] for project in parsed])
-    end = max([project['end'] for project in parsed])
+def calculate(debug, parsed):
+    min_start = min([project['start'] for project in parsed])
+    max_end = max([project['end'] for project in parsed])
 
-    # Each day is only counted once, use a dict of `day: cost`
-    # each day initialized at `0`
-    results = {day: 0 for day in range(start, end+1)}
+    # Each day is only counted once, use a dict of `day: [cost, ..]`
+    results = {day: [] for day in range(min_start, max_end+1)}
     for project in parsed:
-        start_day = project['start']
-        end_day = project['end']
+        start_day  = project['start']
+        end_day    = project['end']
+        full_day   = project['city']['full_day']
         travel_day = project['city']['travel_day']
-        full_day = project['city']['full_day']
-        for day in range(start, end+1):
-            # Gap days will be 0
-            cost = 0
+        for day in range(start_day, end_day+1):
             if day == start_day == end_day:
                 # Assuming 1 day project is full day?
-                print(day, '1_full_day', project['city']['desc'])
-                cost = full_day
+                if debug: print(day, '1_full_day', project['city']['desc'])
+                results[day].append(full_day)
             elif day == start_day:
                 # Assume start is travel day
-                print(day, 'start', project['city']['desc'])
+                if debug: print(day, 'start', project['city']['desc'])
                 cost = travel_day
                 # If we overlapped, take a full day
-                if results[day] > 0:
-                    print("overlap")
+                if len(results[day]) > 0:
+                    if debug: print("overlap")
                     cost = full_day
                 # Or if we worked the previous day
-                elif day-1 > 0 and results[day-1] > 0:
+                elif day-1 > 0 and len(results[day-1]) > 0:
                     # TODO: this doesn't take high cost of previous day (if needed?)
+                    if debug: print("pushed up against")
                     cost = full_day
+                results[day].append(cost)
             elif day == end_day:
                 # Assume end is travel day
-                print(day, 'end', project['city']['desc'])
-                cost = travel_day
+                if debug: print(day, 'end', project['city']['desc'])
+                results[day].append(travel_day)
             elif start_day <= day <= end_day:
                 # Assume full day in the middle
-                print(day, 'middle', project['city']['desc'])
-                cost = full_day
-            # Assuming highest cost?
-            if cost > results[day]:
-                results[day] = cost
-    return results
+                if debug: print(day, 'middle', project['city']['desc'])
+                results[day].append(full_day)
+
+    if debug: print(results)
+    return [max(day) for day in results.values() if len(day) > 0]
 
 def main():
+    debug = sys.argv[1:] # Literally anything
     with open("./sets.yaml") as f:
         sets = yaml.safe_load(f)
-    print("Calculating project sets...")
+    print("Calculating project sets...\n")
     for s in sets['sets']:
-        results = calculate(s, sets['sets'][s])
-        print(results.values(), "=", len(results.values()), "days", "=", sum(results.values()))
-        print("---")
+        if debug: print("---")
+        results = calculate(debug, sets['sets'][s])
+        print(f"{s} {results}", "=", sum(results))
 
 if __name__ == '__main__':
     main()
